0497e3f9cc6d078cd21c3bafd6cb49526c0e0375
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-03-30 12:23:14 +0200

    rendercharacter()

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-document.getElementById("hero").innerHTML = `
-   <div class="character-card">
-      <h4 class="name"> Wizard </h4>
-      <img class="avatar" src="images/wizard.png"/>
-      <p class="health">health: <b> 60 </b></p>
-      <div class="dice-container"><div class="dice"> 6 </div></div>
-   </div> 
-`
+/*
+CHALLENGE
+1. Convert our consts into two objects called 
+"monster" and "hero".
+2. Update the renderCharacter() function so that it accepts 
+a single object "data" as its parameter instead of five string/numbers, 
+reducing the number of arguments to pass in from five to one.
+3. Update the template now each variable is coming from "data".
+4. Update the function call.
+*/
 
-document.getElementById("monster").innerHTML = `
-   <div class="character-card">
-      <h4 class="name"> Orc </h4>
-      <img class="avatar" src="images/orc.png"/>
-      <p class="health">health: <b> 10 </b></p>
-      <div class="dice-container"><div class="dice"> 4 </div></div>
-   </div>
-`
+const hero = {
+   elementId: "hero", 
+   name: "Wizard", 
+   avatar: "images/wizard.png", 
+   health: 60, 
+   diceRoll: 6
+}
+
+const monster = {
+   elementId: "monster", 
+   name: "Orc", 
+   avatar: "images/orc.png", 
+   health: 10, 
+   diceRoll: 4
+}
+
+function renderCharacter(data) {
+   document.getElementById(data.elementId).innerHTML =
+       `<div class="character-card">
+           <h4 class="name"> ${data.name} </h4>
+           <img class="avatar" src="${data.avatar}" />
+           <div class="health">health: <b> ${data.health} </b></div>
+           <div class="dice-container">
+               <div class="dice"> ${data.diceRoll} </div>
+           </div>
+       </div>`
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
