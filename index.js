f4644ab8a103e9636acdad77f4992f8f4afd20ce
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-03-30 19:49:56 +0200

    object.assign

--- index.js
+++ index.js
@@ -30,11 +30,7 @@ const monster = {
 }
 
 function Character(data) {
-   this.elementId = data.elementId
-   this.name = data.name
-   this.avatar = data.avatar
-   this.health = data. health
-   this.diceCount = data.diceCount
+   Object.assign(this, data)
    this.getCharacterHtml = function() {
       const { elementId, name, avatar, health, diceCount } = this;
       const diceHtml = getDiceHtml(diceCount)
@@ -55,15 +51,3 @@ const wizard = new Character(hero)
 const orc = new Character(monster)
 wizard.getCharacterHtml()
 orc.getCharacterHtml()
-
-/*
-Challenge
-1. Create a method called getCharacterHtml that performs the 
-   same tasks as our current renderCharacter function.
-2. Create two new instances of Character. One for a hero, 
-   called "wizard", and one for a monster, called "orc". 
-   Render both of them on the page.
-3. Delete both the old renderCharacter function and the two 
-   lines of code at the bottom of the page which invoke that 
-   function.
-*/
