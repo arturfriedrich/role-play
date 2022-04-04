59581e1c999880593c068b64db8555b6509e5ba1
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-04 23:18:42 +0200

    added getpercentage

--- Character.js
+++ Character.js
@@ -1,11 +1,25 @@
 import {getDiceRollArray, getDicePlaceholderHtml} from "./utils.js"
 
+/*Challenge
+1. Create a property called maxHealth INSIDE Character that 
+stores the maximum health a character can have.
+2. Create an arrow function called getPercentage OUTSIDE 
+Character which takes two parameters, remainingHealth and 
+maximumHealth. getPercentage should return the % of 
+maximumHealth that is remaining.
+3. To test, call getPercentage from within the takeDamage method
+and log out the result. 
+**hint.md for help!!**
+*/
+
+const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth
 
 function Character(data) {
     Object.assign(this, data)
     
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
+    this.maxHealth = this.health
 
     this.takeDamage = function(attackScoreArray) {
         const totalAttackScore = attackScoreArray.reduce((total, current) => total + current)
@@ -14,6 +28,7 @@ function Character(data) {
             this.health = 0
             this.dead = true
         }
+        console.log(getPercentage(this.health, this.maxHealth))
     }
     
     this.getDiceHtml = function () {
