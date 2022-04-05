1294507ad0eba4b4a8bf8559b7674fd87c12c91e
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-04-05 16:08:17 +0200

    bugfix

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 import characterData from './data.js'
 import Character from "./Character.js"
 
+let monstersArray = ["orc", "demon", "goblin"]
+
+/*
+Challenge
+1. Make it so getNewMonster returns a new instance of Character. Think
+what argument you should be passing. If there are no more monsters in the 
+array, getNewMonster should return an empty object {}.
+2. Down near the bottom of the file, set a new variable "monster" equal 
+to our new function getNewMonster.
+3. Delete any code we no longer need.
+- The app will still be broken - don't worry for now!
+**hint.md for help!!**
+*/
+
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
 function attack() {
     wizard.getDiceHtml()
-    orc.getDiceHtml()
-    wizard.takeDamage(orc.currentDiceScore)
-    orc.takeDamage(wizard.currentDiceScore)
-    if ( orc.dead || wizard.dead ) {
+    monster.getDiceHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
+    if ( monster.dead || wizard.dead ) {
         endGame()
     }
    render()
 }
 
 function endGame() {
-   const endMessage = orc.dead && wizard.dead ? "No victors - all creatures are dead" 
+   const endMessage = monster.dead && wizard.dead ? "No victors - all creatures are dead" 
         : wizard.health > 0 ? "The Wizard Wins"
         : "The Orc is Victorious"
     
@@ -31,10 +50,10 @@ function endGame() {
 
 function render() {
     document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-    document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+    document.getElementById("monster").innerHTML = monster.getCharacterHtml();
 }
 
 document.getElementById("attack-button").addEventListener("click", attack)
 const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+let monster = getNewMonster()
 render()
